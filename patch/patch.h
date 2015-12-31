4be32e7777e4373749126833e9614e5d1ad72bfd
DengJinlong <dengjinlong@pingan.com.cn>
2015-12-31 14:29:33 +0800

    Initial Commit

---
+++ patch/patch.h
@@ -0,0 +1,13 @@
+//
+//  patch.h
+//  patch
+//
+//  Created by DengJinlong on 12/31/15.
+//  Copyright © 2015 dengjinlong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface patch : NSObject
+
+@end
---
+++ patch/patch.m
@@ -0,0 +1,13 @@
+//
+//  patch.m
+//  patch
+//
+//  Created by DengJinlong on 12/31/15.
+//  Copyright © 2015 dengjinlong. All rights reserved.
+//
+
+#import "patch.h"
+
+@implementation patch
+
+@end
